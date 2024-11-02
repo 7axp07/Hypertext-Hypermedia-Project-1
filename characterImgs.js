6042649d9f7027bc987285e08f929693221f32e9
alex <przygoda.alex@gmail.com>
2024-11-03 00:47:08 +0100

    characters finished , jquery works

---
+++ characterImgs.js
@@ -0,0 +1,22 @@
+$('#yoosung img').extm({
+    position: 'overlay',
+});
+$('#zen img').extm({
+    position: 'overlay',
+});
+$('#jaehee img').extm({
+    position: 'overlay',
+});
+$('#seven img').extm({
+    position: 'overlay',
+
+});
+$('#jumin img').extm({
+    position: 'overlay',
+});
+$('#jihyun img').extm({
+    position: 'overlay',
+});
+$('#ray img').extm({
+    position: 'overlay',
+});
