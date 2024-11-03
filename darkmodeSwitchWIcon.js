3b132e0bf3b8923f6fcc12069f25f54097906daa
alex <przygoda.alex@gmail.com>
2024-11-03 17:37:33 +0100

    local storage works, dark mode works

--- darkmodeSwitchWIcon.js
+++ darkmodeSwitchWIcon.js
@@ -1,16 +1,19 @@
 const toggleButton = document.getElementById('darkModeSwitch');
 const back = document.getElementById("content");
+const icon = document.getElementById("galleryIcon");
 
 let darkMode = localStorage.getItem("darkMode");
 
 const enableDarkMode = () => {
     back.classList.add("dark");
+    icon.classList.add("dark");
     toggleButton.classList.remove("dark-mode-toggle");
     localStorage.setItem("darkMode", "enabled");
 };
 
 const disableDarkMode = () => {
     back.classList.remove("dark");
+    icon.classList.remove("dark");
     toggleButton.classList.add("dark-mode-toggle");
     localStorage.setItem("darkMode", "disabled");
 };
