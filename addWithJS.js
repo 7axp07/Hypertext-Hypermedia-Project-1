2e28965d41a791758b9ce73bb8aa6de6cba57300
alex <przygoda.alex@gmail.com>
2024-11-05 13:00:47 +0100

    video gets added with JS

--- addWithJS.js
+++ addWithJS.js
@@ -1,2 +1,18 @@
-const video = document.getElementById("videoFrame");
+const vidDiv = document.getElementById("vidDiv");
+const video = document.createElement("iframe");
 
+video.setAttribute("id", "videoFrame");
+
+video.setAttribute("src", "https://www.youtube.com/embed/0XAlNvurr2U?si=Ee1GpdUqKfq3mrTM");
+video.setAttribute("width", "560");
+video.setAttribute("height", "315");
+video.setAttribute("title", "YouTube video player");
+video.setAttribute("frameborder", "0");
+video.setAttribute("allow", "accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share");
+video.setAttribute("referrerpolicy", "strict-origin-when-cross-origin");
+//video.setAttribute("allowfullscreen");
+
+
+vidDiv.appendChild(video);
+
+// < title="YouTube video player" frameborder="0" allow="" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
